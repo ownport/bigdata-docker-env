57d9ea96980136f4d8c06adceac5879fdd33fa51
ownport <ownport@gmail.com>
2016-03-04 00:25:01 +0200

    [*] update installation script for runit

--- install/alpine/runit.sh
+++ install/alpine/runit.sh
@@ -3,6 +3,8 @@
 set -eo pipefail
 [[ "$TRACE" ]] && set -x || :
 
+echo "= /install/alpine/runit.sh ============================================"
+
 echo "http://dl-4.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
 apk add --update runit && \
 rm -rf /var/cache/apk/* 
